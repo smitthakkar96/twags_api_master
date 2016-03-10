d7097dcbbfa4140f49b5174ac10d6ee6ebcd8b7f
smit thakkar <sarangkachhadiya@gmail.com>
2016-03-10 19:01:37 +0530

    initial commit

---
+++ sentiments.py
@@ -0,0 +1,8 @@
+import urllib
+
+def getSentiments(tweet):
+    data = urllib.urlencode({"text": tweet})
+    u = urllib.urlopen("http://text-processing.com/api/sentiment/", data)
+    the_page = u.read()
+    return the_page
+
---
+++ settings.py
@@ -0,0 +1,2 @@
+api_key = "4hVGmHeldwkJXbiofi0yVdJyH"
+api_secret = "VyfWdnCYcCynRO8qjt1ukgOdU277AoobFhu3uvkXt0Gby4Hv3y"
